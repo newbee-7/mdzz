4f650be9e3fd2dc07267c358e0fb4375b23967ec
sakura <392285478@qq.com>
2016-10-10 19:45:05 +0800

    工具主体

--- mdzz/mdzz.py
+++ mdzz/mdzz.py
@@ -5,16 +5,16 @@ from __future__ import print_function, unicode_literals
 import re
 import sys
 import getopt
+import logging
 from xml.dom import minidom
 from collections import namedtuple
 try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
-
 from termcolor import colored
 
-KEY = '7DAB37504E0E78CC3DBED6840B156B06'
+KEY = 'E0F0D336AF47D3797C68372A869BDBC5'
 URL = 'http://dict-co.iciba.com/api/dictionary.php'
 TAG = namedtuple('TAG', 'value color')
 TAG_DICT = {
@@ -26,10 +26,12 @@ TAG_DICT = {
     'acceptation': TAG('%s', 'yellow')
 }
 
+logger = logging.getLogger(__name__)
+
 #连接API
-def getResponse():
+def getResponse(words):
 	try:
-		response = urlopen(URL + '?key=' + KEY + 'w=' + words)
+		response = urlopen(URL + '?key=' + KEY + '&w=' + words)
 	except :
 		print('哎呀，出错了！')
 		return
@@ -44,7 +46,7 @@ def readXml(xml):
 def show(node):
     if not node.hasChildNodes():
         if node.nodeType == node.TEXT_NODE and node.data != '\n':
-            tag_name = node.parentNode.tagName	#获取子节点标签
+            tag_name = node.parentNode.tagName
             content = node.data.replace('\n', '')
             if tag_name in TAG_DICT.keys():
                 tag = TAG_DICT[tag_name]
@@ -55,18 +57,17 @@ def show(node):
 
 #主程序运行
 def main():
-    #提供参数命令
     try:
         options, args = getopt.getopt(sys.argv[1:], ["help"])
     except getopt.GetoptError as e:
         pass
-	#设置命令格式
+
     match = re.findall(r'[\w.]+', " ".join(args).lower())
     words = "_".join(match)
-    response = get_response(words)
+    response = getResponse(words)
     if not response:
         return
-    root = read_xml(response)
+    root = readXml(response)
     show(root)
 
 
